f6a76e0f207670524066a28362af09ee65a2277c
christian <christiancamilo1221@gmail.com>
2017-09-28 10:52:12 -0500

    ca2

--- app/src/main/java/com/i005114/taller2/Parser/JsonCountry_1.java
+++ app/src/main/java/com/i005114/taller2/Parser/JsonCountry_1.java
@@ -29,8 +29,8 @@ public class JsonCountry_1 {
             country.setPoblacion(item.getString("population"));
 country.setIdioma(item.getString("nativeName"));
 
-           // JSONObject languages = item.getJSONObject("languages");
-//country.setIdioma(languages.getString("nativeName"));
+           JSONObject languages = item.getJSONObject("languages");
+country.setIdioma(languages.getString("nativeName"));
 
             countryList.add(country);
         }
